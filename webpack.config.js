8d05c88521969a4de4ac523767d4767506e36cd2
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2023-12-28 12:45:17 +0100

    chore: Set up initial project structure and config
    - Added package.json with project details and dependencies.
    - Configured webpack for development and production builds.
    - Integrated HTML, SCSS, and JS loaders in webpack.
    - Created basic HTML template.
    - Imported Bootstrap and Bootstrap Icons in SCSS and JS files.
    - Set up image and font handling in webpack.
    - Ensured clean and organized project directory structure.
    - Implemented initial styles using Bootstrap.

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: 'body',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [autoprefixer],
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
